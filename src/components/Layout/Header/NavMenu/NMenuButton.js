afac16d2ac76fdc2cbf2358a64f1ead531c95859
MMousavi <m.mousavi0026@gmail.com>
2024-03-16 07:30:47 +0330

    nav-menu

--- src/components/Layout/Header/NavMenu/NMenuButton.js
+++ src/components/Layout/Header/NavMenu/NMenuButton.js
@@ -1,66 +1,30 @@
 import React, {useState} from 'react';
+import Link from "next/link";
 import ListItem from "@mui/material/ListItem";
 import Typography from "@mui/material/Typography";
-import {NavLink} from "react-router-dom";
-import Menu from "@mui/material/Menu";
-import styles from "./NMenu.module.scss";
+import styles from "./NMenu.module.css";
 
-const NMenuButton = ({children, title, to, elementVertical, elementHorizontal, childrenVertical, childrenHorizontal}) => {
-
-    const [anchorEl, setAnchorEl] = useState(null);
-    const handleHoverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleHoverClose = () => {
-        setAnchorEl(null);
-    };
-    const open = Boolean(anchorEl);
+const NMenuButton = ({children, title, href, className}) => {
 
     return (
         children ? (
-            <>
+            <div className={styles.dropButton}>
                 <ListItem
-                    id="basic-menu"
-                    aria-controls={open ? 'demo-positioned-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onMouseEnter={handleHoverOpen}
                     sx={{width: "fit-content", cursor: "pointer", color: "white"}}
                     className={styles.menuItems}
                 >
                     <Typography textAlign="center" color="white" className={styles.menuItem+" "+styles.whiteColor} fontSize="1rem">{title}</Typography>
                 </ListItem>
-                <Menu
-                    id="basic-menu"
-                    aria-labelledby="demo-positioned-button"
-                    open={open}
-                    anchorEl={anchorEl}
-                    onMouseLeave={handleHoverClose}
-                    anchorOrigin={{vertical: elementVertical, horizontal: elementHorizontal,}}
-                    transformOrigin={{vertical: childrenVertical, horizontal: childrenHorizontal,}}
-                    // disableScrollLock
-                    slotProps={{paper: {className: styles.menuPaper}}}
-                >
+                <div className={styles.dropDown + ' ' + className}>
                     {children}
-                </Menu>
-            </>
+                </div>
+            </div>
         ) : (
-            <NavLink
-                style={({isActive}) =>
-                    isActive ?
-                        {
-                            color: "#01e281",
-                            backgroundColor: 'none'
-                        }
-                        : {
-                            color: "white"
-                        }
-                } to={to}
-            >
+            <Link style={{color: "white"}} href={href}>
                 <ListItem className={styles.menuItems}>
                     <Typography textAlign="center" className={styles.menuItem} fontSize="1rem">{title}</Typography>
                 </ListItem>
-            </NavLink>
+            </Link>
         )
     );
 };
